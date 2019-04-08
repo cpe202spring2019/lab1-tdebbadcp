9f2af1f33c55a473bfab3e46ba8e831e64e6ac02
Trilok Debbad <tdebbad@MacBook-Pro-2.local>
2019-04-07 22:46:54 -0700

    completed the functions

--- lab1.py
+++ lab1.py
@@ -2,14 +2,47 @@
 def max_list_iter(int_list):  # must use iteration not recursion
    """finds the max of a list of numbers and returns the value (not the index)
    If int_list is empty, returns None. If list is None, raises ValueError"""
-   pass
+   if int_list == []:
+      return None 
+   elif int_list == None:
+      raise ValueError
+   else:
+      max = int_list[0]
+      for i in int_list:
+         if i >= max:
+            max = i
+      return max
+
 
 def reverse_rec(int_list):   # must use recursion
    """recursively reverses a list of numbers and returns the reversed list
    If list is None, raises ValueError"""
-   pass
+   if int_list == None:
+      raise ValueError
+   elif int_list == []:
+      return None
+   else:
+      if len(int_list) == 1:
+         return int_list
+      else:
+        #return reverse_rec(int_list[1:]) + [int_list[0]]
+        return [int_list[len(int_list) - 1]] + reverse_rec(int_list[:-1])
+ 
 
 def bin_search(target, low, high, int_list):  # must use recursion
    """searches for target in int_list[low..high] and returns index if found
    If target is not found returns None. If list is None, raises ValueError """
-   pass
+   if int_list == None:
+      raise ValueError
+   else:
+      mid = (high + low) // 2
+      if int_list[mid] == target:
+         return mid
+      elif high == low or len(int_list) == mid:
+         return None
+      elif int_list[mid] > target:
+         #return bin_search(target , low , mid - 1 , int_list)
+         return bin_search(target, low, mid, int_list)
+      else:
+         return bin_search(target, mid + 1, high, int_list)
+
