9b827c5a44fab8f1701c5924b54ee807584dd59b
Trilok Debbad <tdebbad@MacBook-Pro-2.local>
2019-04-07 22:44:08 -0700

    added eq and repr methods

--- location.py
+++ location.py
@@ -8,6 +8,11 @@ class Location:
         self.lon = lon      # longitude in degrees (-180 to 180)
 
 # ADD BOILERPLATE HERE (__eq__ and __repr__ functions)
+    def __eq__(self, other):
+        return type(other) == Location and self.name == other.name and self.lat == other.lat and self.lon == other.lon
+    
+    def __repr__(self):
+        return "Location('" + self.name + "', " + str(self.lat) + ", " + str(self.lon) + ")"
 
 def main():
     loc1 = Location("SLO", 35.3, -120.7)
